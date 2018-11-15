c5aa4050222ccb8d5509ae54922d8da1c8853f50
Dionisia Montanez <dmontanez15@gmail.com>
2018-11-14 19:13:52 -0800

    wave 2 done

--- src/adagrams.js
+++ src/adagrams.js
@@ -33,6 +33,29 @@ const Adagrams = {
     letters.sort(() => Math.random() - 0.5);
     return letters.slice(0, 10);
   },
+  usesAvailableLetters(input, lettersInHand) {
+    const lettersInHandMap = {};
+    // ['A', 'A', 'B', 'N'] => { 'A': 2, 'B': 1, 'N': 1 }
+    lettersInHand.forEach((letter) => {
+      if (lettersInHandMap[letter]) {
+        lettersInHandMap[letter] += 1;
+      }
+      else {
+        lettersInHandMap[letter] = 1;
+      }
+    });
+
+    for (let i = 0; i < input.length; i++) {
+      if (lettersInHandMap[input[i]]) {
+        lettersInHandMap[input[i]] -= 1;
+      }
+      else {
+        return false;
+      }
+    }
+    
+    return true;
+  }
 };
 
 // Do not remove this line or your tests will break!
